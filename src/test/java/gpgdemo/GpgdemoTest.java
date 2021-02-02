cf45b1d9614e0b4cb7fa60c9759644912f15c27e
Jeff Scott Brown <brownj@objectcomputing.com>
2021-02-02 15:00:21 -0600

    demo

---
+++ src/test/java/gpgdemo/GpgdemoTest.java
@@ -0,0 +1,21 @@
+package gpgdemo;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class GpgdemoTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
